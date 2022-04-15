3051d8633c02171cb16a6b786562ae862872c4f2
msyon2 <suyon.korea@gmail.com>
2022-04-15 11:01:16 +0900

    05 React Lesson: state,class,lifecycle

--- src/App.js
+++ src/App.js
@@ -1,85 +1,38 @@
 import React from "react";
-import PropTypes from 'prop-types';
 
-/* function Fruit(props) {
-  console.log(props);
-	return (
-    <h2>Love Me { props.fav }</h2>
-  )
-} */
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    console.log('constructor실행완료');
+  }
 
-//component
-function Fruit({ name, picture, rating }) {
-  return (
+  state = {
+    count: 0,
+  };
+  add = () => {
+    this.setState({count: this.state.count + 1})
+  }
+  minus = () => {
+    this.setState({count: this.state.count - 1})
+  }
+  componentDidMount(){
+    console.log('componentDidMount실완');
+  }
+  componentDidUpdate(){
+    console.log('Update실완');
+  }
+  componentWillUnmount(){
+    console.log('willUnmount실완');
+  }
+  render(){
+    console.log('render실완')
+    return (
     <div>
-      <h2>Love Me {name} </h2>
-      <h4>{rating} / 5.0</h4>
-      <img src={picture} alt={name}/>
-
-    </div>
-  );
-}
-const fruitILike = [
-  //{key키:value값},
-  {
-    id:1,
-    name: "banana",
-    image: "http://qwerew.cafe24.com/images/banana.png",
-    rating:3.5,
-  },
-  {
-    id:2,
-    name: "orange",
-    image: "http://qwerew.cafe24.com/images/orange.png",
-    rating:4.5,
-  },
-  {
-    id:3,
-    name: "apple",
-    image: "http://qwerew.cafe24.com/images/apple.png",
-    rating:4.8,
-  },
-  {
-    id:4,
-    name: "melon",
-    image: "http://qwerew.cafe24.com/images/melon.jpg",
-    rating:3.8,
-  },
-];
-
-/* method 3
-function renderFruit(dish){
-  return<Fruit key={dish.id} name={dish.name} picture={dish.image}/>
-} */
-
-function App() {
-  return (
-    <div>
-      {fruitILike.map((dish)=>(
-        <Fruit key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-      ))}
-
-      {/* method 3
-      {fruitILike.map(renderFruit)} */}
-      
-      {/* method 2
-      {fruitILike.map((dish) => (
-        <Fruit key={dish.id} name={dish.name} picture={dish.image} />
-      ))} */}
-
-      {/* {fruitILike.map(function(dish){
-          return <Fruit name= {dish.name}/> 
-      })}
- */}
-    </div>
-  );
-}
-
-/* prototype = props의 data type 검사 */
-Fruit.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
+      <h1>class component. The number is: {this.state.count} </h1>
+      <button onClick={this.add}>plus</button>
+      <button onClick={this.minus}>minus</button>
+    </div>)
+  }
 }
 
 export default App;
