5cb596940ca358eab701b1abec7405a322163012
msyon2 <suyon.korea@gmail.com>
2022-04-22 12:00:06 +0900

    React MovieApp final version: detail page css

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,29 +1,43 @@
-import React from 'react';
+import React from "react";
+import "./Detail.css"
 
-
-class Detail extends React.Component{
-	componentDidMount(){
-		const{location,history}=this.props;
-		if( location.state === undefined ) {
-			history.push('/');
-		}
-	}
-	render(){
-		const {location}=this.props;
-		if(location.state) {
-			return (
-				<div>
-					<span>{location.state.title}</span><br />
-					<span>{location.state.year}</span><br />
-					<span>{location.state.summary}</span>
-				</div>
-			)
-		}else{
-			return null
-		}
-	}
-
-
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push("/");
+    }
+  }
+  render() {
+    const { location } = this.props;
+    if (location.state) {
+      return (
+        <div className="detail">
+          <img
+            src={location.state.poster}
+            alt={location.state.title}
+            title={location.state.title}
+          />
+          <div className="movie__data">
+            <h3 className="movie__title">{location.state.title}</h3>
+            <h5 className="movie__year">{location.state.year}</h5>
+            <ul className="movie__genres">
+              {location.state.genres.map((genre, index) => {
+                return (
+                  <li key={index} className="movie_gnre">
+                    {genre}
+                  </li>
+                );
+              })}
+            </ul>
+			<p className="movie__summary">{location.state.summary}</p>
+          </div>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
 }
 
 export default Detail;
